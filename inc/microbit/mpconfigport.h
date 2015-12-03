6a1acb1adab638af97267954d76f19a77f55f7ff
Damien George <damien.p.george@gmail.com>
2015-12-03 10:07:16 +0000

    Add neopixel module to global list.

--- inc/microbit/mpconfigport.h
+++ inc/microbit/mpconfigport.h
@@ -89,12 +89,14 @@ extern const struct _mp_obj_module_t microbit_module;
 extern const struct _mp_obj_module_t this_module;
 extern const struct _mp_obj_module_t antigravity_module;
 extern const struct _mp_obj_module_t love_module;
+extern const struct _mp_obj_module_t neopixel_module;
 
 #define MICROPY_PORT_BUILTIN_MODULES \
     { MP_OBJ_NEW_QSTR(MP_QSTR_microbit), (mp_obj_t)&microbit_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_this), (mp_obj_t)&this_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_antigravity), (mp_obj_t)&antigravity_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_love), (mp_obj_t)&love_module }, \
+    { MP_OBJ_NEW_QSTR(MP_QSTR_neopixel), (mp_obj_t)&neopixel_module }, \
     \
     /* the following provide aliases for existing modules */ \
     { MP_OBJ_NEW_QSTR(MP_QSTR_collections), (mp_obj_t)&mp_module_collections }, \
